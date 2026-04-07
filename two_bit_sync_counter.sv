e081946c8344ba56fb6a1dce8f61df6f4cb50644
Cihat Özlük <cihat.ozluk@std.yeditepe.edu.tr>
2026-04-07 12:35:51 +0300

    Add files via upload

---
+++ two_bit_sync_counter.sv
@@ -0,0 +1,40 @@
+module two_bit_sync_counter(
+input logic x,clk,
+output logic [2:0] q,
+output logic y);
+
+initial
+begin
+q=2'b00;
+y=1'b0;
+end
+
+always_ff @(posedge clk) begin  // sync olmsının sebebi?
+
+//{y,q}<=q+x;  //overflow ile output 
+
+q[0]<=~q[0];
+end
+
+always_ff @(negedge q[0]) begin  // sync olmsının sebebi?
+
+//{y,q}<=q+x;  //overflow ile output 
+
+q[1]<=~q[1];
+end
+
+always_ff @(negedge q[1]) begin  // sync olmsının sebebi?
+
+//{y,q}<=q+x;  //overflow ile output 
+
+q[2]<=~q[2];
+end
+
+
+
+
+
+
+
+
+endmodule
